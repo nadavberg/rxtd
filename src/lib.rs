34ab6753b9c2e1a99710e92611b6fdf00e2be9e0
Nadav Berg <nadavberg@gmail.com>
2026-03-15 23:24:03 +0200

    2026-03-15

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-#![allow(warnings, unused)]
+// #![allow(warnings, unused)]
 
 use serde::Deserialize;
 use serde::Serialize;
-
 use hound;
 
 
@@ -12,17 +11,10 @@ pub struct RxPreset {
     #[serde(rename = "@name")]
     pub name: String,
     
-    // #[serde(rename = "@author")]
-    // author: Option<String>,
-    
-    // #[serde(rename = "@comment")]
-    // comment: Option<String>,
-    
     #[serde(rename = "$value")]
     pub tags: Vec<RxTag>,
 }
 
-// Enum to Catch Interleaved Tags:
 #[derive(Debug, Deserialize)]
 pub enum RxTag {
     #[serde(rename = "PARAM")]
@@ -106,8 +98,6 @@ pub struct IntermediatePreset {
     pub volume: f64,
     pub velocity: f64,
     pub layout: bool,
-    pub bank: f64,
-    pub mode: f64,
     pub pads: [IntermediatePad; 32],
 }
 impl IntermediatePreset {
@@ -119,8 +109,6 @@ impl IntermediatePreset {
             volume: 0.699999988079071,
             velocity: 0.0,
             layout: false,
-            bank: 0.0, // delete?
-            mode: 0.0, // delete?
             pads: std::array::from_fn(|i| {IntermediatePad::new(i)}),
         }
     }
@@ -136,6 +124,64 @@ impl IntermediatePreset {
                     pad_index += 1;
                 }
             }
+        }
+    }
+
+    pub fn set_truncate_range_and_fix_other_stuff(&mut self) {
+        for pad in &mut self.pads {
+            if pad.inactive {continue}
+            if pad.sample_reversed {
+                let temp = pad.play_range_start;
+                pad.play_range_start = 1.0 - pad.play_range_end;
+                pad.play_range_end = 1.0 - temp;
+                let temp = pad.fade_in;
+                pad.fade_in = 1.0 - pad.fade_out;
+                pad.fade_out = 1.0 - temp;
+            }
+
+            // Fix fades:
+            if pad.play_range_start > 0.0 || pad.play_range_end < 1.0 {
+                let factor = 1.0 / (pad.play_range_end - pad.play_range_start);
+                pad.fade_in = (pad.fade_in - pad.play_range_start) * factor;
+                pad.fade_out = 1.0 - (pad.play_range_end - pad.fade_out) * factor;
+            }
+
+            // Check file and calculate trancate start/end:
+            let wav = match hound::WavReader::open(&pad.sample_path) {
+                Ok(w) => w,
+                Err(e) => {
+                    println!("\tProblem with {}: {}", pad.sample_path, e);
+                    continue
+                },
+            };
+            let sample_length = wav.duration() as f64;
+            if sample_length < 1.0 {
+                println!("\tZero length sample! ({})", pad.sample_path);
+                continue
+            }
+
+            let truncate_start = pad.sample_start as f64 / sample_length;
+            let truncate_end = pad.sample_end as f64 / sample_length;
+            let factor = truncate_end - truncate_start;
+
+            // Fix start/end points:
+            if factor < 1.0 {
+                pad.play_range_start = truncate_start + factor * pad.play_range_start;
+                pad.play_range_end = truncate_start + factor * pad.play_range_end;
+            }
+            
+            // Fix loop points:
+            if pad.sample_reversed {
+                let temp = pad.loop_range_start;
+                pad.loop_range_start = truncate_end - factor * pad.loop_range_end;
+                pad.loop_range_end = truncate_end - factor * temp;
+            } else {
+                pad.loop_range_start = truncate_start + factor * pad.loop_range_start;
+                pad.loop_range_end = truncate_start + factor * pad.loop_range_end;   
+            }  
+            
+            pad.truncate_start = truncate_start;
+            pad.truncate_end = truncate_end;
         }
     }
 }
@@ -175,6 +221,9 @@ pub struct IntermediatePad {
 
     pub midikey: u8,
     pub color: u8,
+
+    pub truncate_start: f64,
+    pub truncate_end: f64,
 }
 impl IntermediatePad {
     pub fn new(i: usize) -> Self {
@@ -214,7 +263,134 @@ impl IntermediatePad {
 
             midikey: 36 + i,
             color: 0,
+
+            truncate_start: 0.0,
+            truncate_end: 1.0,
         }
+    }
+}
+
+
+
+
+pub fn build_intermediate_preset(rx_preset: RxPreset) -> IntermediatePreset {
+    let mut intermediate_preset = IntermediatePreset::new();
+    for tag in rx_preset.tags {
+        match tag {
+            RxTag::Param(param) => process_param_tag(&param, &mut intermediate_preset),
+            RxTag::Samples(samples) => process_samples_container(&samples, &mut intermediate_preset),
+            RxTag::Gui(gui) => process_gui_container(&gui, &mut intermediate_preset),
+        }
+    }
+    intermediate_preset.assign_midi_keys();
+    intermediate_preset.set_truncate_range_and_fix_other_stuff();
+    intermediate_preset.name = rx_preset.name;
+    intermediate_preset
+}
+
+pub fn pad_id_to_index(pad_id: &str) -> usize {
+    let bytes = pad_id.as_bytes();
+    let bank = (bytes[0] - b'a') as usize;
+    let pad = (bytes[1] - b'1') as usize;
+    8 * bank + pad
+}
+
+pub fn process_param_tag(param: &RxParam, intermediate_preset: &mut IntermediatePreset) {
+    let value = match param.value {
+        Some(v) => v,
+        None => return
+    };
+    
+    if !param.id.contains('_') {
+        match param.id.as_str() {
+            "volume" => intermediate_preset.volume = value,
+            "velocity" => intermediate_preset.velocity = value,
+            "layout" => intermediate_preset.layout = (value != 0.0) as bool,
+            _ => (),
+        }
+        return;
+    }
+    
+    let (a, b) = param.id.split_once('_').unwrap();
+    
+    // Polyphony:
+    if b.len() == 1 {
+        let index = (b.as_bytes()[0] - b'1') as usize;
+        intermediate_preset.polyphony[index] = value as u8;
+        return;
+    }
+
+    let (param_name, pad_id): (&str, &str);
+
+    if a.len() == 2 {
+        pad_id = a;
+        param_name = b;
+    } else {
+        pad_id = b;
+        param_name = a;
+    }
+
+    let pad_index: usize = pad_id_to_index(pad_id);
+    let ref mut pad = intermediate_preset.pads[pad_index];
+
+    match param_name {
+        "pitch" => pad.pitch = (15.0 * value).round() as u8,
+        "decay" => pad.decay = value,
+        "level" => pad.level = (15.0 * value).round() as u8,
+        "pan" => pad.pan = value,
+        "pad" => pad.pad = value,
+        "output" => pad.output = value as u8,
+        "filter" => pad.filter = value as u8,
+        "finetune" => pad.finetune = value,
+        "gain" => pad.gain = value,
+        "mono" => pad.mono = value as u8,
+        "speed" => pad.speed = value as u8,
+        "loop_mode" => pad.loop_mode = value as u8,
+        "loop_range_end" => pad.loop_range_end = value,
+        "loop_range_start" => pad.loop_range_start = value,
+        "play_range_end" => pad.play_range_end = value,
+        "play_range_start" => pad.play_range_start = value,
+        "fade_in" => pad.fade_in = value,
+        "fade_out" => pad.fade_out = value,
+        _ => ()
+    }
+}
+
+pub fn process_samples_container(samples: &Samples, intermediate_preset: &mut IntermediatePreset) {
+    for sample in &samples.items {
+        let pad_index: usize = pad_id_to_index(&sample.id);
+        let ref mut pad = intermediate_preset.pads[pad_index];
+        
+        let Some(references) = &sample.references else {
+            pad.inactive = true;
+            continue
+        };
+        
+        let Some(reference) = &references.reference else {
+            pad.inactive = true;
+            continue
+        };
+        
+        pad.sample_path = match reference.ref_type.as_str() {
+            "productCommonData" => format!(r"C:/ProgramData/Inphonik/RX1200{}", reference.value),
+            _ => reference.value.clone(),
+        };     
+        
+        pad.sample_reversed = sample.reversed;
+        pad.sample_gain = sample.gain;
+        pad.sample_start = sample.start;
+        pad.sample_end = sample.end;
+    }
+}
+
+pub fn process_gui_container(gui: &RxGui, intermediate_preset: &mut IntermediatePreset) {
+    for g in gui.params.iter() {
+        if !g.id.contains('_') { continue }
+        if g.value.is_none() { continue }
+        let value = g.value.unwrap();
+        let pad_id = g.id.split_once('_').expect("It's fine!").1;
+        let pad_index: usize = pad_id_to_index(pad_id);
+        intermediate_preset.pads[pad_index].color = (value * 7.0).round() as u8;
     }
 }
 
@@ -327,138 +503,6 @@ pub struct  TdMapping {
 
     #[serde(rename = "@loopmode")]
     pub loopmode: u8,
-}
-
-pub fn build_intermediate_preset(rx_preset: RxPreset) -> IntermediatePreset {
-    let mut intermediate_preset = IntermediatePreset::new();
-    for tag in rx_preset.tags {
-        match tag {
-            RxTag::Param(param) => process_param_tag(&param, &mut intermediate_preset),
-            RxTag::Samples(samples) => process_samples_container(&samples, &mut intermediate_preset),
-            RxTag::Gui(gui) => process_gui_container(&gui, &mut intermediate_preset),
-        }
-    }
-    intermediate_preset.assign_midi_keys();
-    intermediate_preset.name = rx_preset.name;
-    intermediate_preset
-}
-
-pub fn pad_id_to_index(pad_id: &str) -> usize {
-    let bytes = pad_id.as_bytes();
-    let bank = (bytes[0] - b'a') as usize;
-    let pad = (bytes[1] - b'1') as usize;
-    8 * bank + pad
-}
-
-pub fn process_param_tag(param: &RxParam, intermediate_preset: &mut IntermediatePreset) {
-    if param.value.is_none() { return; }
-    let value = param.value.unwrap();
-
-    if !param.id.contains('_') {
-        match param.id.as_str() {
-            "volume" => intermediate_preset.volume = value,
-            "velocity" => intermediate_preset.velocity = value,
-            "layout" => intermediate_preset.layout = (value != 0.0) as bool,
-            _ => (),
-        }
-        return;
-    }
-    
-    let (a, b) = param.id.split_once('_').unwrap();
-    
-    // Polyphony:
-    if b.len() == 1 {
-        let index = (b.as_bytes()[0] - b'1') as usize;
-        intermediate_preset.polyphony[index] = value as u8;
-        return;
-    }
-
-    let (param_name, pad_id): (&str, &str);
-
-    if a.len() == 2 {
-        pad_id = a;
-        param_name = b;
-    } else {
-        pad_id = b;
-        param_name = a;
-    }
-
-    let pad_index: usize = pad_id_to_index(pad_id);
-    let ref mut pad = intermediate_preset.pads[pad_index];
-
-    match param_name {
-        "pitch" => pad.pitch = (15.0 * value).round() as u8,
-        "decay" => pad.decay = value,
-        "level" => pad.level = (15.0 * value).round() as u8,
-        "pan" => pad.pan = value,
-        "pad" => pad.pad = value,
-        "output" => pad.output = value as u8,
-        "filter" => pad.filter = value as u8,
-        "finetune" => pad.finetune = value,
-        "gain" => pad.gain = value,
-        "mono" => pad.mono = value as u8,
-        "speed" => pad.speed = value as u8,
-        "loop_mode" => pad.loop_mode = value as u8,
-        "loop_range_end" => pad.loop_range_end = value,
-        "loop_range_start" => pad.loop_range_start = value,
-        "play_range_end" => pad.play_range_end = value,
-        "play_range_start" => pad.play_range_start = value,
-        "fade_in" => pad.fade_in = value,
-        "fade_out" => pad.fade_out = value,
-        _ => ()
-    }
-}
-
-pub fn process_samples_container(samples: &Samples, intermediate_preset: &mut IntermediatePreset) {
-    for sample in &samples.items {
-        let pad_index: usize = pad_id_to_index(&sample.id);
-        let ref mut pad = intermediate_preset.pads[pad_index];
-        
-        if sample.references.is_none() {pad.inactive = true; continue}
-        let references = sample.references.as_ref().unwrap();
-        if references.reference.is_none() {pad.inactive = true; continue}
-        let reference = references.reference.as_ref().unwrap();
- 
-        pad.sample_reversed = sample.reversed;
-        pad.sample_gain = sample.gain;
-        pad.sample_start = sample.start;
-        pad.sample_end = sample.end;
-        
-        let sample_path =
-            if reference.ref_type == "productCommonData" {
-                r"C:/ProgramData/Inphonik/RX1200".to_string() + reference.value.as_str()
-            } else {
-                reference.value.clone()
-            };
-        pad.sample_path = sample_path.clone();
-
-        let wav = hound::WavReader::open(&sample_path);
-        if wav.is_err() {
-            println!("{} {}", wav.err().unwrap(), sample_path.rsplit_once('\\').expect("feugh...").1);
-            continue
-        }
-        pad.sample_length = wav.unwrap().duration();
-    }
-}
-
-pub fn process_gui_container(gui: &RxGui, intermediate_preset: &mut IntermediatePreset) {
-    for g in gui.params.iter() {
-        if g.value.is_none() { continue }
-        let value = g.value.unwrap();
-        if !g.id.contains('_') {
-            // delete? (just continue)
-            match g.id.as_str() {
-                "bank" => intermediate_preset.bank = value,
-                "mode" => intermediate_preset.mode = value,
-                _ => (),
-            }
-            continue;
-        }
-
-        let (_, pad_id) = g.id.split_once('_').expect("foo...");
-        let pad_index: usize = pad_id_to_index(pad_id);
-        intermediate_preset.pads[pad_index].color = (value * 7.0).round() as u8;
-    }
 }
 
 
@@ -579,128 +623,64 @@ pub fn rx_master_volume_to_td_master_volume(rx_master_volume: f64) -> (f64, f64)
     (td_master_volume, td_pad_volume_adjustment)
 }
 
-pub fn fix_play_range_loop_fade_and_get_trunc(pad: &mut IntermediatePad) -> (f64, f64) {
-    // println!("start:{}\tend:{}", pad.play_range_start, pad.play_range_end);
-    if pad.sample_reversed {
-        let temp = pad.play_range_start;
-        pad.play_range_start = 1.0 - pad.play_range_end;
-        pad.play_range_end = 1.0 - temp;
-        let temp = pad.fade_in;
-        pad.fade_in = 1.0 - pad.fade_out;
-        pad.fade_out = 1.0 - temp;
-    }
+pub fn rx_mono_to_td_mono_outputmode(rx_mono: u8) -> (u8, u8) {
+    if rx_mono == 1 { return (0, 0) }
+    (1, match rx_mono { 2 => 1, 3 => 2, _ => 0 })
+}
 
-    // println!("in:{}\tout:{}", pad.fade_in, pad.fade_out);
-    if pad.play_range_start > 0.0 || pad.play_range_end < 1.0 {
-        let factor = 1.0 / (pad.play_range_end - pad.play_range_start);
-        pad.fade_in = (pad.fade_in - pad.play_range_start) * factor;
-        pad.fade_out = 1.0 - (pad.play_range_end - pad.fade_out) * factor;
-    }
-        
-    let sample_length = pad.sample_length as f64;
-    let td_truncate_start = pad.sample_start as f64 / sample_length;
-    let td_truncate_end = pad.sample_end as f64 / sample_length;
-
-    // Fix start/end points:
-    let length_as_ratio = td_truncate_end - td_truncate_start;
-    if length_as_ratio < 1.0 {
-        pad.play_range_start = td_truncate_start + length_as_ratio * pad.play_range_start;
-        pad.play_range_end = td_truncate_start + length_as_ratio * pad.play_range_end;
-        
-    }
-    
-    // Fix loop points:
-    if pad.sample_reversed {
-        let temp = pad.loop_range_start;
-        pad.loop_range_start = td_truncate_end - length_as_ratio * pad.loop_range_end;
-        pad.loop_range_end = td_truncate_end - length_as_ratio * temp;
-    } else {
-        pad.loop_range_start = td_truncate_start + length_as_ratio * pad.loop_range_start;
-        pad.loop_range_end = td_truncate_start + length_as_ratio * pad.loop_range_end;   
-    }  
-        
-        (td_truncate_start, td_truncate_end)
-    }
-    
-
-
-pub fn build_td_preset(preset: IntermediatePreset) -> TdPreset {
+pub fn build_td_preset(intermediate_preset: IntermediatePreset) -> TdPreset {
     let mut td_pads = TdPads { items: Vec::new() };
     let mut pad_count: u8 = 0;
-    let td_velocity = rx_velocity_to_td_velocity(preset.velocity);
-    let (td_master_volume, td_volume_adjustment) = rx_master_volume_to_td_master_volume(preset.volume);
+    let td_velocity = rx_velocity_to_td_velocity(intermediate_preset.velocity);
+    let (td_master_volume, td_volume_adjustment) = rx_master_volume_to_td_master_volume(intermediate_preset.volume);
     
-    for mut pad in preset.pads {
+    for pad in intermediate_preset.pads {
         if pad.inactive {continue}
-        // println!("{pad:?}\n");
+        pad_count += 1;
 
         let td_color = rx_color_to_td_color(pad.color);
         let td_volume = rx_level_and_gain_to_td_volume(pad.level, pad.gain);
-        let (td_truncate_start, td_truncate_end) = fix_play_range_loop_fade_and_get_trunc(&mut pad);
         let (td_tune, td_finetune) = rx_pitch_speed_finetune_to_td_tune_finetune(pad.pitch, pad.speed, pad.finetune);
-
         let td_fadein = f64::min(2.0 * pad.fade_in, 1.0);
         let td_fadeout = f64::min(2.0 * (1.0 - pad.fade_out), 1.0);
-
         let td_loopenable = (pad.loop_mode > 0) as u8;
         let td_pingpong = (pad.loop_mode > 1) as u8;
-        
-        let td_mono: u8;
-        let td_outputmode: u8;
-        if pad.mono == 1 {
-            td_mono = 0;
-            td_outputmode = 0;
-        } else {
-            td_mono = 1;
-            td_outputmode = match pad.mono {
-                0 => 0,
-                2 => 1,
-                3 => 2,
-                _ => 0,
-            }
-        };
+        let (td_mono, td_outputmode) = rx_mono_to_td_mono_outputmode(pad.mono);
 
-        let td_pad = TdPad {
+        td_pads.items.push(TdPad {
             version: 13,
             color: td_color,
             volume: td_volume,
             pan: pad.pan,
             midikey: pad.midikey,
-            mappings: TdMappings {
-                mapping:TdMapping {
-                    path: pad.sample_path,
-                    start: pad.play_range_start,
-                    end: pad.play_range_end,
-                    loopstart: pad.loop_range_start,
-                    loopend: pad.loop_range_end,
-                    fadein: td_fadein,
-                    fadeout: td_fadeout,
-                    truncatestart: td_truncate_start,
-                    truncateend: td_truncate_end,
-                    volume: td_volume_adjustment,
-                    tune: td_tune,
-                    finetune: td_finetune,
-                    mono: td_mono,
-                    outputmode: td_outputmode,
-                    velocityintensity: td_velocity,
-                    reversenabled: pad.sample_reversed as u8,
-                    normalizesample: 0,
-                    loopenabled: td_loopenable,
-                    loopmode: td_pingpong,
-                }
-            }
-        };
-        td_pads.items.push(td_pad);
-        pad_count += 1;
+            mappings: TdMappings { mapping:TdMapping {
+                path: pad.sample_path,
+                start: pad.play_range_start,
+                end: pad.play_range_end,
+                loopstart: pad.loop_range_start,
+                loopend: pad.loop_range_end,
+                fadein: td_fadein,
+                fadeout: td_fadeout,
+                truncatestart: pad.truncate_start,
+                truncateend: pad.truncate_end,
+                volume: td_volume_adjustment,
+                tune: td_tune,
+                finetune: td_finetune,
+                mono: td_mono,
+                outputmode: td_outputmode,
+                velocityintensity: td_velocity,
+                reversenabled: pad.sample_reversed as u8,
+                normalizesample: 0,
+                loopenabled: td_loopenable,
+                loopmode: td_pingpong,
+            }}
+        });
     }
 
-    let td_preset = TdPreset {
+    TdPreset {
         version: 13,
-        // name: preset.name,
         volume: td_master_volume,
         numberofpadsmode: (pad_count > 16) as u8,
         pads: td_pads,
-    };
-
-    td_preset
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
-#![allow(warnings, unused)]
+// #![allow(warnings, unused)]
 
 use rxtd::*;
-use std::ffi::OsStr;
-// use std::fs;
-use std::io;
-use std::path::Path;
-use std::path::PathBuf;
+use std::fs;
+use std::path::{Path, PathBuf};
+use quick_xml::{de, se};
 
-// use quick_xml::events::{Event, BytesDecl};
-// use quick_xml::Writer;
-// use std::io::{BufWriter, Write};
 
-#[derive(Debug)]
-struct RxFile {
-    path: std::path::PathBuf,
-    name: String,
-}
-
-// fn get_files_in_directory(directory_path: &str) -> Vec<RxFile> {
-fn get_files_in_directory(directory_path: &str) -> Vec<std::path::PathBuf> {
+fn get_files_in_directory(directory_path: impl AsRef<Path>) -> Vec<PathBuf> {
     let mut files = Vec::new();
 
-    let directory = match std::fs::read_dir(directory_path) {
+    let directory = match fs::read_dir(directory_path) {
         Ok(dir) => dir,
         Err(i) => {
             println!("Bad directory! {i}");
@@ -69,55 +57,20 @@ fn main() {
 
     println!("Found {} RX1200 presets:", rx_files.len());
 
-    for file in rx_files {
-        let file_name = match file.file_stem().and_then(|name| name.to_str()) {
+    for rx_file in rx_files {
+        let file_name = match rx_file.file_stem().and_then(|name| name.to_str()) {
             Some(name) => name,
             None => continue,
         };
-        print!("{}, ", file_name);
+        let rx_xml = fs::read_to_string(&rx_file).expect("Couldn't read source file");
+        let rx_preset: RxPreset = de::from_str(&rx_xml).expect("Failed to parse preset");
+        let intermediate_preset = build_intermediate_preset(rx_preset);
+        let td_preset = build_td_preset(intermediate_preset);
+        let td_xml = se::to_string(&td_preset).expect("Failed to serialize preset");
+        let td_xml = format!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n{}", td_xml);
+        let td_file_path = format!("converted_presets\\{file_name}.taldrum");
+        fs::write(&td_file_path, td_xml).expect("Failed to write file");
+        println!("{}, ", td_file_path);
     }
     println!();
-
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\Factory Collection\Alive and Kickin.rx1200";
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\Factory Collection\Brighton.rx1200";
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\Factory Collection\Young Blood 808.rx1200";
-
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\User Collection\a.rx1200";
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\User Collection\Alive and Kickinz.rx1200";
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\User Collection\One Sine.rx1200";
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\User Collection\stere0.rx1200";
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\User Collection\sine_pitch.rx1200";
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\User Collection\Beat.rx1200";
-    let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\User Collection\Loops.rx1200";
-
-    // let file_path = r"C:\Users\Nadav\AppData\Roaming\Inphonik\RX1200\Collections\Templates Collection\All clear.rx1200";
-
-    let xml_data = std::fs::read_to_string(file_path).expect("Couldn't read source file");
-
-    let file_name = file_path
-        .rsplit_once('\\')
-        .unwrap()
-        .1
-        .rsplit_once('.')
-        .unwrap()
-        .0
-        .to_string();
-
-    let rx_preset: RxPreset =
-        quick_xml::de::from_str(&xml_data).expect("Failed to parse RX1200 preset");
-
-    let intermediate_preset = build_intermediate_preset(rx_preset);
-
-    let td_preset = build_td_preset(intermediate_preset);
-
-    let raw_xml = quick_xml::se::to_string(&td_preset).expect("Failed to serialize preset");
-    let final_xml = format!("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n{}", raw_xml);
-
-    let mut td_file_path = "converted_presets\\".to_string();
-    td_file_path.push_str(&file_name);
-    td_file_path.push_str(".taldrum");
-    // let td_file_path = file_name + ".taldrum";
-
-    std::fs::write(&td_file_path, final_xml).expect("Failed to write file");
-    println!("{file_name}\n");
 }
